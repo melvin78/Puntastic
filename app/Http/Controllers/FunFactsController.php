c10975d5a1b388fc4ce3f9df7ad6b8ae30c1e38d
melvin <melvin.mark@centrino.co.ke>
2022-11-09 15:15:36 +0300

    fix: use jenssegers eloquent model

--- app/Http/Controllers/FunFactsController.php
+++ app/Http/Controllers/FunFactsController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FunFacts;
 use Illuminate\Http\Request;
 
 class FunFactsController extends Controller
 {
     public function index($id)
     {
-
+      return FunFacts::all();
     }
 
 }
--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
         /*
          * Application Service Providers...
          */
