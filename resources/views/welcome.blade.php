71097104f83e92109472187d7750a3656a09b303
melvin <melvin.mark@centrino.co.ke>
2023-01-11 15:21:50 +0300

    feat: add title icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Chat Bot</title>
+
+    <link rel="icon" href="/images/chat.png">
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
